846ff3fab1347374251a960f42d8ab3cdbfe721f
pajecurumin <114811881+pajecurumin@users.noreply.github.com>
2025-03-14 21:19:16 -0300

    Add files via upload

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from 'react';
+import { View, FlatList, ActivityIndicator, StyleSheet } from 'react-native';
+import ProductCard from '../components/ProductCard';
+
+const HomeScreen = ({ navigation }) => {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch('https://dummyjson.com/products')
+      .then(response => response.json())
+      .then(data => {
+        setProducts(data.products);
+        setLoading(false);
+      })
+      .catch(error => console.error(error));
+  }, []);
+
+  if (loading) {
+    return <ActivityIndicator size="large" color="#fff" style={styles.loading} />;
+  }
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={products}
+        keyExtractor={(item) => item.id.toString()}
+        numColumns={2} // Define duas colunas
+        contentContainerStyle={styles.list}
+        renderItem={({ item }) => (
+          <ProductCard 
+            product={item} 
+            onPress={() => navigation.navigate('DetailsScreen', { product: item })} 
+          />
+        )}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fffff', // Azul escuro
+    paddingHorizontal: 10, 
+  },
+  list: {
+    justifyContent: 'space-between', // Distribui os cards uniformemente
+    marginTop: 10, // Adiciona margem no topo para não ficar muito colado no topo da tela
+  },
+  loading: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default HomeScreen;
